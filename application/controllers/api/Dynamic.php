340440d4837c87f2168dabed06cc60aa8e7f7762
sopian <sopian@eluon.com>
2024-11-03 01:44:01 +0700

    remove type month or daily

--- application/controllers/api/Dynamic.php
+++ application/controllers/api/Dynamic.php
@@ -18,19 +18,11 @@ class Dynamic extends REST_Controller {
     public function getHeaderData_get() {
         
         $process_type = $this->get('proc_type');
-        $type = $this->get('type');
-        $processType=array();
-        $processType = $this->sikat_profile_type_model->get($process_type);   
-        $id = $processType->ID;
         $dynamicData = array();
-        if($id != null){
-            $where = array(
-                "type" => $type,
-                "process_type" => $id
-            );
-            $dynamicData=$this->sikat_profile_indikator_model->get_where($where);
-        }   
-
+        $where = array(
+            "process_type" =>  $process_type
+        );
+        $dynamicData=$this->sikat_profile_indikator_model->get_where($where);
         $this->set_response($dynamicData, REST_Controller::HTTP_OK);
     }
 
